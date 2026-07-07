6733c096db3fe60ba48dc194bf48e1ab9889df5d
anvith <anwithgowda3@gmail.com>
2026-07-07 19:56:39 +0530

    project setup

---
+++ src/test/java/com/anv/AiStudentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudentAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
